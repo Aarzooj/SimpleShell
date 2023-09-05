5d2c8d3b32e39269e460500ac5beeb4190e7d2ad
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-09-06 00:40:43 +0530

    Update simple-shell.c

--- simple-shell.c
+++ simple-shell.c
@@ -7,46 +7,66 @@
 #include <pthread.h>
 #include <sys/wait.h>
 
-int create_process_and_run(char *command) {
+int create_process_and_run(char *command)
+{
     // In a real implementation, you would use fork/exec or system() to run the command.
     // Here, we'll just print the command and return a dummy status.
     printf("Running command: %s\n", command);
 
     // trying fork
     int status = fork();
-    if (status<0){
+    if (status < 0)
+    {
         printf("error");
-    }else if (status == 0){
+    }
+    else if (status == 0)
+    {
         printf("child\n");
-    }else{
+    }
+    else
+    {
         printf("parent\n");
     }
     return 0;
+    // return status;
 }
 
-int launch (char *command) {
- int status;
- status = create_process_and_run(command);
- return status;
-}
-
-char* read_user_input() {
-    char* input = NULL;
-    size_t bufsize = 0;
-    getline(&input, &bufsize, stdin);
-    return input;
-}
-
-void shell_loop() {
+int launch(char *command)
+{
     int status;
-    do {
-        printf("iiitd@possum:~$ ");
-        char* command = read_user_input();
-        status = launch(command);
-    } while(status);
+    status = create_process_and_run(command);
+    return status;
 }
 
-int main(){
+void shell_loop()
+{
+    int status;
+    do
+    {
+        printf("iiitd@possum:~$ ");
+        char *command = NULL;
+        size_t bufsize = 0;
+        getline(&command, &bufsize, stdin);
+        char **args = (char **)malloc(256 * sizeof(char *));
+        int countArgs = 0;
+
+        char *split = strtok(command, " ");
+        while (split != NULL)
+        {
+            args[countArgs++] = split;
+            split = strtok(NULL, " ");
+        }
+        // for (int i = 0; args[i] != NULL; i++)
+        // {
+        //     printf("%s\n", args[i]);
+        // }
+
+        status = launch(command);
+    } while (status);
+}
+
+int main()
+{
     shell_loop();
     return 0;
 }
