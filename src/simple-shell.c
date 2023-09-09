0bbe0dacd1ea7ed75cfa4607df2f5570e9531cdc
Anushka Srivastava <anushka22086@iiitd.ac.in>
2023-09-10 00:32:39 +0530

    comments added

--- src/simple-shell.c
+++ src/simple-shell.c
@@ -6,14 +6,19 @@
 #include <time.h>
 #include <stdbool.h>
 
+// defining sizes for data structures allocated
 #define INPUT_SIZE 256
 #define HISTORY_SIZE 100
 #define MAX_BGPROCESS 5
 
+// PIDs of background process running in the background. Can handle max of 5 processes only
+// initialising all values to 0
 pid_t running_bg_process[MAX_BGPROCESS] = {0};
 
+// flag if the background process is started
 int bgProcess = 0;
 
+// struct for each command stored in history
 struct CommandParameter
 {
     char command[INPUT_SIZE];
@@ -31,12 +36,14 @@ struct CommandHistory
 
 struct CommandHistory history;
 
+// Function to display details of each command when the program is terminated using Ctrl + C
 void displayTerminate()
 {
     printf("--------------------------------\n");
     for (int i = 0; i < history.historyCount; i++)
     {
         struct CommandParameter record = history.record[i];
+        // conversion of start and end time to string structures
         struct tm *start_time_info = localtime(&record.start_time);
         char start_time_buffer[80];
         strftime(start_time_buffer, sizeof(start_time_buffer), "%Y-%m-%d %H:%M:%S", start_time_info);
@@ -49,13 +56,15 @@ void displayTerminate()
     }
 }
 
+// SIGINT (Ctrl + C) handler
 static void my_handler(int signum)
 {
     printf("\n");
     displayTerminate();
-    exit(1);
+    exit(0);
 }
 
+// displaying command history
 void displayHistory()
 {
     history.record[history.historyCount].process_pid = getpid();
@@ -65,15 +74,18 @@ void displayHistory()
     }
 }
 
+// append background process as the latest value in the array
 int append(pid_t pid)
 {
     int added = -1;
+    // if no background process running then add it as the first value
     if (running_bg_process[0] == 0 && running_bg_process[1] == 0 && running_bg_process[2] == 0 && running_bg_process[3] == 0 && running_bg_process[4] == 0)
     {
         added = 0;
         running_bg_process[0] = pid;
         return added;
     }
+    // adding the process only after the last non-zero PID that is a running background process
     for (int i = MAX_BGPROCESS - 2; i >= 0; i--)
     {
         if (running_bg_process[i] != 0)
@@ -85,8 +97,9 @@ int append(pid_t pid)
     return added;
 }
 
-pid_t pop(pid_t pid)
+int pop(pid_t pid)
 {
+    // removing the PID of the completed background process and resetting it to 0
     for (int i = 0; i < MAX_BGPROCESS; i++)
     {
         if (running_bg_process[i] == pid)
@@ -95,35 +108,37 @@ pid_t pop(pid_t pid)
             return i;
         }
     }
+    return -1;
 }
 
+// running shell commands
 int create_process_and_run(char **args)
 {
     int status = fork();
     if (status < 0)
     {
-        printf("Error");
+        perror("Fork Failed");
     }
     else if (status == 0)
     {
-
         int check = execvp(args[0], args);
         if (check == -1)
         {
-            printf("Error running execvp system call\n");
+            printf("%s: command not found\n", args[0]);
             return -1;
         }
     }
     else
     {
+        // Checking for background process
         if (!(bgProcess))
         {
             int child_status;
-            wait(&child_status); // Wait for the child to complete
+            // Wait for the child to complete
+            wait(&child_status);
             if (WIFEXITED(child_status))
             {
                 int exit_code = WEXITSTATUS(child_status);
-                // printf("Child process exited with status: %d\n", exit_code);
             }
             else
             {
@@ -132,14 +147,17 @@ int create_process_and_run(char **args)
         }
         else
         {
+            // parent doesn't wait for the background child processes to terminate
             int order = append(status);
             if (order != -1)
             {
                 history.record[history.historyCount].process_pid = status;
+                // start of background process
                 printf("[%d] %d\n", order + 1, status);
             }
             else
             {
+                history.record[history.historyCount].process_pid = 0;
                 printf("No more background processes can be added");
             }
         }
@@ -147,6 +165,7 @@ int create_process_and_run(char **args)
     return status;
 }
 
+// launch function
 int launch(char **args)
 {
     int status;
@@ -162,12 +181,13 @@ int launch(char **args)
     return status;
 }
 
+// taking input from the terminal
 char *read_user_input()
 {
     char *input = (char *)malloc(INPUT_SIZE);
     if (input == NULL)
     {
-        perror("Error in malloc");
+        perror("Can't allocate memory\n");
         free(input);
         exit(EXIT_FAILURE);
     }
@@ -184,6 +204,7 @@ char *read_user_input()
     }
 }
 
+// function to strip the leading and trailing spaces
 char *strip(char *string)
 {
     char stripped[strlen(string) + 1];
@@ -215,13 +236,20 @@ char *strip(char *string)
     }
     stripped[len] = '\0';
     char *final_strip = (char *)malloc(INPUT_SIZE);
+    if (final_strip == NULL){
+        perror("Memory allocation failed\n");
+    }
     memcpy(final_strip, stripped, INPUT_SIZE);
     return final_strip;
 }
 
+// splitting the command according to specified delimiter
 char **tokenize(char *command, const char delim[2])
 {
     char **args = (char **)malloc(INPUT_SIZE * sizeof(char *));
+    if (args == NULL){
+        perror("Memory allocation failed\n");
+    }
     int count = 0;
     char *token = strtok(command, delim);
     while (token != NULL)
@@ -229,18 +257,21 @@ char **tokenize(char *command, const char delim[2])
         args[count++] = strip(token);
         token = strtok(NULL, delim);
     }
+    // checking for & as the last argument to check if the process should be in the background
     if (count > 0 && strcmp(args[count - 1], "&") == 0 && strcmp(delim, " ") == 0)
     {
         bgProcess = 1;
-        free(args[count - 1]); // Remove the "&" from the args list
+        free(args[count - 1]);
         args[count - 1] = NULL;
         count--;
     }
     return args;
 }
 
+// Handling Piping
 int pipe_process(char **cmds, int pipes)
 {
+    // Creating pipes according to the input
     int fd[pipes][2];
     for (int i = 0; i < pipes; i++)
     {
@@ -250,18 +281,21 @@ int pipe_process(char **cmds, int pipes)
         }
     }
     int pid;
+    // Using the read and write descriptors of the pipe for each command to read from STDIN and write to STDOUT
     for (int i = 0; i < pipes + 1; i++)
     {
         char **args = tokenize(cmds[i], " ");
         pid = fork();
         if (pid < 0)
         {
-            perror("error\n");
+            perror("Fork failed\n");
         }
         else if (pid == 0)
         {
+            // First command cannot read from the pipe
             if (i > 0)
             {
+                // Closing all the descriptors which are not required
                 for (int j = 0; j < pipes; j++)
                 {
                     if (j != i)
@@ -273,23 +307,35 @@ int pipe_process(char **cmds, int pipes)
                         close(fd[j][0]);
                     }
                 }
-                dup2(fd[i - 1][0], STDIN_FILENO);
+                if (dup2(fd[i - 1][0], STDIN_FILENO) == -1)
+                {
+                    printf("Pipe %d: Reading failed\n", i - 1);
+                    return -1;
+                }
+                // Closing the used read descriptor
                 close(fd[i - 1][0]);
             }
+            // Last command cannot write into the pipe. It can only write to STDOUT
             if (i < pipes)
             {
-                dup2(fd[i][1], STDOUT_FILENO);
+                if (dup2(fd[i][1], STDOUT_FILENO) == -1)
+                {
+                    printf("Pipe %d: Writing failed\n", i);
+                    return -1;
+                }
+                // Closing the used write descriptor
                 close(fd[i][1]);
             }
             int check = execvp(args[0], args);
             if (check == -1)
             {
-                printf("Error running execvp system call\n");
+                printf("%s: command not found\n", args[0]);
                 return -1;
             }
         }
         else
         {
+            // Closing the duplicate file descriptors created due to forking
             if (i > 0)
             {
                 close(fd[i - 1][0]);
@@ -300,17 +346,21 @@ int pipe_process(char **cmds, int pipes)
             }
         }
     }
+    // waiting for all the child process to terminate
     for (int i = 0; i < pipes + 1; i++)
     {
         wait(NULL);
     }
+    // returning pid of the latest child process
     return pid;
 }
 
+// launch process for pipe commands
 int launch_pipe(char *command)
 {
     int status;
     int count = 0;
+    // counting number of pipes
     for (int i = 0; command[i] != '\0'; i++)
     {
         if (command[i] == '|')
@@ -318,6 +368,7 @@ int launch_pipe(char *command)
             count++;
         }
     }
+    // splitting all user commands according to the pipe
     char **cmds = tokenize(command, "|");
     status = pipe_process(cmds, count);
     if (status > 0)
@@ -328,47 +379,64 @@ int launch_pipe(char *command)
     {
         history.record[history.historyCount].process_pid = 0;
     }
+    // setting the time after the pipe process terminates
     history.record[history.historyCount].end_time = time(NULL);
-
     history.record[history.historyCount].duration = difftime(
         history.record[history.historyCount].end_time,
         history.record[history.historyCount].start_time);
+    // freeing allocated memory space
     free(cmds);
     return status;
 }
 
+// Handling SIGCHLD: Passed when a child process terminates (background process in this case)
 void handle_sigchld(int signum)
 {
     int status;
     pid_t pid;
-
+    // returns the pid of the terminated child
+    // WNOHANG: returns 0 if the status information of any process is not available i.e. the process has not terminated
+    // In other words, the loop will only run after the background processes running has terminated or changed state
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
     {
-        // printf("Background process with PID %d terminated\n", pid);
+        // Finding the child process in history according to the PID of the terminated process
         for (int i = 0; i < history.historyCount; i++)
         {
             if (history.record[i].process_pid == pid)
             {
-                history.record[i].end_time = time(NULL);
-
-                history.record[i].duration = difftime(
-                    history.record[i].end_time,
-                    history.record[i].start_time);
+                // finding from background process array
                 int order = pop(pid);
-                char *tmp = strdup(history.record[i].command);
-                if (tmp == NULL)
+                if (order != -1)
                 {
-                    perror("Error in strdup");
-                    exit(EXIT_FAILURE);
+                    history.record[i].end_time = time(NULL);
+                    history.record[i].duration = difftime(
+                        history.record[i].end_time,
+                        history.record[i].start_time);
+                    // duplicating the command to tmp to avoid corruption of data
+                    char *tmp = strdup(history.record[i].command);
+                    if (tmp == NULL)
+                    {
+                        perror("Error in strdup");
+                        exit(EXIT_FAILURE);
+                    }
+                    tmp = strtok(tmp, "&");
+                    printf("\n[%d]+ Done                    %s\n", order + 1, tmp);
+                    break;
                 }
-                tmp = strtok(tmp, "&");
-                printf("\n[%d]+ Done                    %s\n", order + 1, tmp);
-                break;
+                else
+                {
+                    // background process not found i.e. it was not added to the array
+                    history.record[i].end_time = history.record[i].start_time;
+                    history.record[i].duration = difftime(
+                        history.record[i].end_time,
+                        history.record[i].start_time);
+                }
             }
         }
     }
 }
 
+// Checking for quotation marks and backslash in the input
 bool validate_command(char *command)
 {
     if (strchr(command, '\\') || strchr(command, '\"') || strchr(command, '\''))
@@ -378,12 +446,15 @@ bool validate_command(char *command)
     return false;
 }
 
+// main shell loop
 void shell_loop()
 {
+    // Setting the function for SIGINT (Ctrl + C)
     if (signal(SIGINT, my_handler) == SIG_ERR)
     {
         perror("SIGINT handling failed");
     }
+    // Setting the function for SIGCHLD
     if (signal(SIGCHLD, handle_sigchld) == SIG_ERR)
     {
         perror("SIGCHLD handling failed");
@@ -391,6 +462,7 @@ void shell_loop()
     int status;
     do
     {
+        // Creating the prompt text
         char *user = getenv("USER");
         if (user == NULL)
         {
@@ -401,17 +473,20 @@ void shell_loop()
         int hostname = gethostname(host, sizeof(host));
         if (hostname == -1)
         {
-            perror("gethostname");
+            perror("HOST not declared");
             exit(1);
         }
         printf("%s@%s~$ ", user, host);
 
+        // taking input
         char *command = read_user_input();
+        // handling the case if the input is blank or enter key
         if (strlen(command) == 0 || strcmp(command, "\n") == 0)
         {
             status = 1;
             continue;
         }
+        // removing the newline character
         command = strtok(command, "\n");
         bool isInvalidCommand = validate_command(command);
         char *tmp = strdup(command);
@@ -430,9 +505,10 @@ void shell_loop()
                 history.record[history.historyCount].end_time,
                 history.record[history.historyCount].start_time);
             history.historyCount++;
-            printf("Invalid Command : includes quotes/backslach\n");
+            printf("Invalid Command : includes quotes/backslash\n");
             continue;
         }
+        // checking if the input is "history"
         if (strstr(command, "history"))
         {
             if (history.historyCount > 0)
@@ -455,6 +531,7 @@ void shell_loop()
         }
         else
         {
+            // checking for pipes in the process
             if (strchr(command, '|'))
             {
                 strcpy(history.record[history.historyCount].command, tmp);
@@ -469,19 +546,21 @@ void shell_loop()
                 history.record[history.historyCount].start_time = time(NULL);
                 status = launch(args);
                 history.record[history.historyCount].end_time = time(NULL);
-
                 history.record[history.historyCount].duration = difftime(
                     history.record[history.historyCount].end_time,
                     history.record[history.historyCount].start_time);
                 history.historyCount++;
             }
         }
+        // resetting the background process variable
         bgProcess = 0;
     } while (status);
 }
 
+// Main function
 int main()
 {
+    // initializing count for elements in history
     history.historyCount = 0;
     shell_loop();
     return 0;
